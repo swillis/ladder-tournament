0ab332b33aa1e660c7104f4ee19924be922c778e
James Shedden <james@jshedden.com>
2015-01-05 21:54:53 +0000

    Updating app to bootstrap angular and include browserify-shim

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,20 @@ var angular = require('angular');
 
 require('./controllers/ng-welcome-controller'); 
 
-angular.module('myApp', [
+var app = angular.module('app', [
 	'ngWelcomeCtrl'
 ]);
+
+function isSupportedBrowser() {
+  var hasJSON = 'JSON' in window && 'parse' in JSON;
+  var supportMode = location.search.match(/supportMode/);
+  return hasJSON && !supportMode;
+}
+
+angular.element(document).ready(function setup() {
+  // Only give decent browser a js experience
+  if (isSupportedBrowser()) {
+    // Bootstrap Angular
+    angular.bootstrap(document, ['app']);
+  }
+});
